9df9bbb342fccb0f281051900cd50f5fbc662338
Sean <hello@imsean.me>
2019-08-07 23:23:27 -0400

    update home page with welcome hero and remove starter code

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,12 @@ import { Link } from "gatsby"
 import Layout from "../layouts/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import Hero from "../components/hero";
+import WelcomeHero from "../components/welcome-hero";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Hero></Hero>    
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <WelcomeHero />
   </Layout>
 )
 
