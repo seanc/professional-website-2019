c2687895ff7c244925be6acc0cfba4d828a32d32
Sean <hello@imsean.me>
2019-08-07 23:23:42 -0400

    add portfolio-item component

---
+++ src/components/portfolio-item.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PortfolioItem = () => (
+  <div className="portfolio-item">
+    <div className="overlay">
+
+    </div>
+  </div>
+)
+
+PortfolioItem.propTypes = {}
+
+export default PortfolioItem
