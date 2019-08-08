17e33f9ab0ac961914573f7af2971c7ea9466bb5
Sean <hello@imsean.me>
2019-08-07 23:22:48 -0400

    make hero component reusable

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,26 +1,18 @@
 import React from 'react'
-import '../styles/hero.scss'
+import PropTypes from 'prop-types'
 
-const Hero = () => {
-  return (
-    <>
-      <div className="hero">
-        <div className="container mx-auto">
-          <h1 className="font-black text-6xl">Good Night,</h1>
-          <h3 className="font-bold text-4xl mt-5">My name's <span className="text-gray-500">Sean</span>.</h3>
-
-          <p className="text-2xl max-w-md">
-            I'm a full-stack web developer with <span className="font-bold text-gray-500 underline">6 years of work experience.</span>
-            {` `}I'm working on <span className="font-bold text-gray-500 underline">12 projects</span>
-          </p>
-        </div>
+const Hero = (children) => (
+  <>
+    <div className="hero">
+      <div className="container">
+        {children}
       </div>
-    </>
-  )
+    </div>
+  </>
+)
+
+Hero.propTypes = {
+  children: PropTypes.node.isRequired
 }
 
-Hero.defaultProps = {
-
-}
-
-export default Hero;
+export default Hero
